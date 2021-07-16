89078a0cee1d09587f636e90f5d00f8557535353
thewhiteh4t <lohityapushkar@gmail.com>
2021-07-16 07:13:30 +0530

    alpha loaded

---
+++ js/msg.js
@@ -0,0 +1,9 @@
+console.log(`%c
+┌── MESSAGE FROM THEWHITEH4T ────────────────────────────────────────────────────┐
+│                                                                                │
+│   Hello, me and my team have spent countless hours in designing this website   │
+│   Please don't copy it, make something original from scratch                   │
+│   It is not that hard really!!                                                 │
+│                                                                                │
+└────────────────────────────────────────────────────────────────────────────────┘
+`, "color: cyan; background: black; font-family: monospace; font-size: 18px; line-height: 1");
